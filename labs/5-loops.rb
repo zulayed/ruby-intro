bd8d4b06e486a32a8b2979d2115846eee4b4b2a6
zulayed <ali.zulayed@kellogg.northwestern.edu>
2025-01-29 18:10:50 -0600

    Initial commit

---
+++ labs/5-loops.rb
@@ -0,0 +1,31 @@
+# To run this code, be sure your current working directory
+# is the same as where this file is located and then run:
+# ruby 5-loops.rb
+
+# EXERCISE
+# Build a deck of cards. Given the following arrays of card data,
+# use a loop to write out the cards to the screen.
+clubs = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
+diamonds = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
+hearts = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
+spades = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
+
+# Sample output:
+# 2 of Clubs
+# 3 of Clubs
+# 4 of Clubs
+# ...etc
+# Queen of Spades
+# King of Spades
+# Ace of Spades
+
+# CHALLENGE #1
+# The arrays are identical for each suit and can be simplified by using
+# a ranks array as seen below. Try to complete the exercise again by
+# combining these arrays.
+ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
+suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
+
+# CHALLENGE #2
+# Deal a poker hand. Shuffle the deck and "deal" (i.e. display) a 5 card hand (i.e. 5 cards from the deck).
+# You will want to look at the documentation for Arrays: https://ruby-doc.org/core-2.7.0/Array.html
---
+++ labs/6-weather.rb
@@ -0,0 +1,32 @@
+# To run this code, be sure your current working directory
+# is the same as where this file is located and then run:
+# ruby 6-weather.rb
+# EXERCISE
+# Given the following Ruby hash that represents weather data,
+# write a weather summary out to the screen. Something like this:
+# Currently it is 67 degrees and Sunny.
+# Forecast for the next 7 days:
+# - 65 degres and Mostly Cloudy
+# - 70 degrees and Partly Cloudy
+# - 62 degrees and Sunny
+# - 68 degrees and Sunny
+# - 65 degrees and Partly Cloudy with a 70% chance of rain
+# - 72 degrees and Partly Cloudy
+# - 60 degrees and Rainy
+# Note: only display the chance of rain if above 50%.
+# Note: do not display the chance of rain if conditions are already rainy.
+weather_data = {
+  current: {
+    temperature: 67,
+    conditions: "Sunny"
+  },
+  forecast: [
+    { temperature: 65, conditions: "Mostly Cloudy", precipitation: 0.4 },
+    { temperature: 70, conditions: "Partly Cloudy", precipitation: 0.2 },
+    { temperature: 62, conditions: "Sunny", precipitation: 0.1 },
+    { temperature: 68, conditions: "Sunny", precipitation: 0.1 },
+    { temperature: 65, conditions: "Partly Cloudy", precipitation: 0.7 },
+    { temperature: 72, conditions: "Partly Cloudy", precipitation: 0.4 },
+    { temperature: 60, conditions: "Rainy", precipitation: 0.9 }
+  ]
+}
