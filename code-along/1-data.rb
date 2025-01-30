7bf515c5e556179368c094f8c89b52df461256eb
Md Ali Zulayed <ali.zulayed@kellogg.northwestern.edu>
2025-01-30 01:13:58 +0000

    going along with class

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -1,19 +1,61 @@
-# To run this code, be sure your current working directory
-# is the same as where this file is located and then run:
-# ruby 1-data.rb
+# # To run this code, be sure your current working directory
+# # is the same as where this file is located and then run:
+# # ruby 1-data.rb
 
-# In Ruby, there are different *types* of data:
+# # In Ruby, there are different *types* of data:
 
-# Numbers
+# # Numbers
 
-# Non-numbers
+# puts 2
+# puts 5
 
-# True, False
+# puts 5 + 2
+# puts 5 / 2.0
 
-# Nothing
+# # Non-numbers
+# puts "Hello world"
+# puts "tacos"*3
 
-# Variables
+# puts "tacos:"+ 3.to_s
 
-# Combine strings and variables
+# # True, False
+# puts true
+# puts false
+
+
+# # Nothing
+
+# puts nil
+
+# # Variables
+# x = 5
+# y = 4
+# z = 21
+# puts x+y
+
+# food = "tacos "
+# quantity = 3
+
+# puts food*quantity
+
+# # Combine strings and variables
+
+# first_name = "Boba"
+# greeting = "Hello, #{first_name}"
+
+# puts greeting
+
 
 # String manipulation
+
+puts "Hello".length
+puts "how are you?".capitalize
+
+x = "computer"
+
+puts x.reverse
+puts x.upcase
+puts x.downcase
+puts x.swapcase
+
+puts "hello \"my\" name is Sheum"
--- code-along/2-conditionals.rb
+++ code-along/2-conditionals.rb
@@ -1,15 +1,35 @@
-# To run this code, be sure your current working directory
-# is the same as where this file is located and then run:
-# ruby 2-conditionals.rb
+# # To run this code, be sure your current working directory
+# # is the same as where this file is located and then run:
+# # ruby 2-conditionals.rb
 
-# Booleans
+# # Booleans
+# test_is_true = true
+# puts test_is_true
+# # Boolean Expressions
 
-# Boolean Expressions
+# puts 3==2
+# puts 3!=2
+# puts 3>2
+# puts 3<2
 
-# If Conditional Logic
+# # If Conditional Logic
+
+# if 3==2
+#     puts "This text should never display"
+
+#     #== is the syntax for 'if equals'
 
 # If/Else Conditional Logic
+
+if 3 < 2 
+    puts "text is no"
+else
+    puts "this is the text man"
+end
 
 # Elsif Conditional Logic
 
 # Combining Expressions
+
+# AND && 
+# OR ||
